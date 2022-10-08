19683934c43ce5623985199efe2231da61ba7fa0
Artem <artem.anikin.2015@gmail.com>
2022-10-08 14:10:11 +0200

    feat EH-2129: add todoItem

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,20 @@
+import { ITodoData } from "../types/dataItem"
+
+import './TodoItem.scss'
+
+const TodoItem:React.FC<ITodoData> = (props) => {
+
+    const {id, title} = props;
+
+    return <div className="todo-item" key={id}>
+        <div className="item-input">
+            <input type="checkbox" />
+        </div>
+        {title}
+        <div className="item-btns">
+            <button> Edit </button>
+            <button> Delete</button>
+        </div>
+    </div>
+}
+export {TodoItem}
